7d5c3817ed535ee6be5afa863c5d215ddc143aea
tsion11 <11@afrika.dev>
2023-05-26 16:15:03 +0300

    title and keywords

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,14 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Sassio - NextJS Template by AliThemes</title>
+        <title>
+          Samsung Frame Art - a collection of art specifically created for
+          Samsung Frame TVs
+        </title>
+        <meta
+          name="keywords"
+          content="samsamsung frame,samsung the frame, samsung art, art samsung, art samsung frame, frame art, the frame, frame tv, samsung tv, samsung tv art, samsung abstract,samsung digital, african, animals, architecture, art, black and white, city, fashion, food, kids, landscape, nature, , people, photography, vintage, classics, AI, AI art, AI generate, Generative art, surreal, Abstract, Abstract art, Abstract painting, Abstract photography, Abstract realism, Abstract series, Abstract structure, Surrealism "
+        />
       </Head>
       <NextNProgress />
       <Component {...pageProps} />
